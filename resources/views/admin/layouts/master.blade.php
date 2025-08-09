b6ba83bfb83254173a71bf446530d343dd609133
Fitrah Maulana <58702720+fitrahmaulana@users.noreply.github.com>
2025-08-09 12:12:42 +0700

    Fix dropdown links

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -47,7 +47,7 @@
             <div class="collapse navbar-collapse justify-content-end">
             <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                <a class="nav-link" href="javscript:void(0)" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link" href="javascript:void(0)" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i class="material-icons">person</i>
                     <p class="d-lg-none d-md-block">
                     Some Actions
